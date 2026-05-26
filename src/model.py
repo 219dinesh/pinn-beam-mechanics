7d0992ba8842e6d024bd34a48d77f3fc49b4124f
219dinesh <134425342+219dinesh@users.noreply.github.com>
2026-05-26 10:46:04 +0545

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,18 @@
+import torch
+import torch.nn as nn
 
+class BeamPINN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(1, 32),
+            nn.Tanh(),
+            nn.Linear(32, 32),
+            nn.Tanh(),
+            nn.Linear(32, 32),
+            nn.Tanh(),
+            nn.Linear(32, 1)
+        )
+
+    def forward(self, x):
+        return self.net(x)
